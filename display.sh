89c93098bfad36fd9a9091f89a0b04949ebdf390
Boomaa23 <cmo93003@yahoo.com>
2020-02-02 17:55:38 -0800

    Add network and display utils

---
+++ display.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dp_conn="$(cat /sys/class/drm/card0-DP-1/status)"
+lvds_act="$(cat /sys/class/drm/card0-LVDS-1/enabled)"
+
+if [ "$dp_conn" == "connected" -a "$lvds_act" == "enabled" ]; then
+	xrandr --output LVDS1 --off --output DP1 --auto 
+	echo "Switched display to external monitor"
+fi
+if [ "$dp_conn" == "disconnected" -a "$lvds_act" == "disabled" ]; then
+	xrandr --output DP1 --off --output LVDS1 --auto	
+	echo "Switched display to internal monitor"
+fi
